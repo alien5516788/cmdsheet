04587790a1eb538eed037057ab1b27b836617f45
alien5516788 <93880176+alien5516788@users.noreply.github.com>
2026-03-18 02:13:02 +0530

    Initialized a python project and moved vite project to src-vite.

---
+++ src-vite/src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="h-30 bg-blue-400">
+      <p>Hello there</p>
+    </div>
+  );
+}
--- src-vite/vite.config.ts
+++ src-vite/vite.config.ts
@@ -5,4 +5,8 @@ import babel from "@rolldown/plugin-babel";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
 });
