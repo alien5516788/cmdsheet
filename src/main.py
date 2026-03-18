a7ac926a5c297172ade0042d548d66eb5dc04c7a
alien5516788 <93880176+alien5516788@users.noreply.github.com>
2026-03-18 11:27:24 +0530

    Added dev mode to main python file.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
+import os
+
+import webview
+
+DEV = True
+
+if DEV:
+    url = "http://localhost:5173/"
+else:
+    base_dir = os.path.dirname(__file__)
+    url = os.path.join(base_dir, "dist", "index.html")
+
+
 def main():
-    print("Hello world")
+    webview.create_window("App", url)
+    webview.start(gui="qt")
 
 
 if __name__ == "__main__":
