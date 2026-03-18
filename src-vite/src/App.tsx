57d6e7d0afe301f377adce3902afd4039e0dcc9b
alien5516788 <93880176+alien5516788@users.noreply.github.com>
2026-03-18 14:16:59 +0530

    Configured tailwind css.

--- src-vite/src/App.tsx
+++ src-vite/src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="h-30 bg-red-400">
+    <div className="h-30 bg-blue-400">
       <p>Hello there</p>
     </div>
   );
